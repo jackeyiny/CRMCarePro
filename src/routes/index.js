831ed00e3e692ab04ed5248f1cbf330f70eadb79
TRANHOANGTUAN <90249402+jackeyiny@users.noreply.github.com>
2025-02-21 15:28:29 +0700

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,14 @@ const UserRouter = require('./UserRouter')
 const ProductRouter = require('./ProductRouter')
 const CustomerRouter = require('./CustomerRouter')
 const AdminRouter = require('./AdminResponseRouter')
+const WebhookRouter = require('./WebhookRouter')
 // nơi chứa tất cả route của api
 const routes = (app) => {
     app.use('/api/user', UserRouter),
     app.use('/api/product', ProductRouter),
     app.use('/api/customer', CustomerRouter),
-    app.use('/api/admin', AdminRouter)
+    app.use('/api/admin', AdminRouter),
+    app.use('/api/webhook', WebhookRouter)
 
 }
 
